5862be3953c515656346603ae94aee066643bae0
ABskr <bskr26@gmx.de>
2021-03-26 15:05:32 +0100

    spacing and sizing cards

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -3,7 +3,7 @@ import { Card, Col } from 'react-bootstrap';
 const SingleCard = ({ item }) => {
   return (
     <Col>
-      <Card className='' style={{ width: '15rem' }}>
+      <Card className='mb-1' style={{ width: '8rem' }}>
         <Card.Img variant='top' src={item.Poster} />
       </Card>
     </Col>
