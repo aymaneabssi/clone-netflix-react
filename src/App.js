5a42d2296a4dc9a025525d12e5c574ed515bdc3c
ardigermenji <ardi.germenji@gmail.com>
2021-03-26 11:54:37 +0100

    fetches data from omdb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    movies: [],
+  };
 
-export default App;
+  componentDidMount = async () => {
+    // console.log('app mounted');
+    const apiKey = '95717d44';
+    const movies = ['harry potter', 'the lord of the rings', 'breaking bad'];
+    const endpointAllData = `http://www.omdbapi.com/?apikey=${apiKey}&s=`;
+
+    try {
+      movies.forEach(async (movie) => {
+        const resp = await fetch(endpointAllData + movie.replaceAll(' ', '+'));
+        if (resp.ok) {
+          console.log('resp ok');
+          const data = await resp.json();
+          console.log(data.Search);
+          this.setState({ movies: [...this.state.movies, data.Search] });
+          console.log(this.state.movies);
+        } else {
+          console.log('something went wrong');
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>hello</h1>
+        </div>
+      </>
+    );
+  }
+}
