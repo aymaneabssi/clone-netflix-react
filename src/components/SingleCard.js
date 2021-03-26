05ce211a01dd5f29d859f3f77be370728195052c
ardigermenji <ardi.germenji@gmail.com>
2021-03-26 16:44:22 +0100

    saijdosa

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -4,11 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 const SingleCard = ({ item }) => {
   return (
-    <Col key={uuidv4()}>
-      <Card className='mb-1' style={{ width: '8rem' }}>
-        <Card.Img variant='top' src={item.Poster} />
-      </Card>
-    </Col>
+    <>
+      <Col key={uuidv4()}>
+        <Card className='mb-1' style={{ width: '8rem' }}>
+          <Card.Img variant='top' src={item.Poster} />
+        </Card>
+      </Col>
+    </>
   );
 };
 
