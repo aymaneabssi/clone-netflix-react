12c64ea7650ae452a93439b01922abd7cbf56983
ardigermenji <ardi.germenji@gmail.com>
2021-03-26 15:13:42 +0100

    implement fetch on search and error handling from api

--- src/App.js
+++ src/App.js
@@ -3,21 +3,61 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Header from './components/Header';
-import { Form, Container } from 'react-bootstrap';
+import { Form, Container, Alert } from 'react-bootstrap';
 
 export default class App extends Component {
   state = {
     query: '',
+    queriedElement: [],
     movies: [],
+    queryNotFound: false,
+    queryErrorFromApi: '',
   };
 
   handleInput = (e) => {
-    this.setState({ ...this.state, query: e.currentTarget.value });
+    this.setState({
+      ...this.state,
+      query: e.currentTarget.value,
+      queryNotFound: false,
+    });
   };
 
   handleSubmit = async (e) => {
-    e.prevenDefault();
-    console.log(e);
+    e.preventDefault();
+    console.log(this.state.query);
+    try {
+      const resp = await fetch(
+        `http://www.omdbapi.com/?apikey=95717d44&s=${this.state.query
+          .toLowerCase()
+          .replaceAll(' ', '+')}`
+      );
+      if (resp.ok) {
+        // this api resp always with ok so i have to check data.Response
+        console.log('resp ok');
+        const data = await resp.json();
+        if (data.Response === 'True') {
+          // console.log(data);
+          // i have data and change state
+          this.setState({
+            ...this.state,
+            queriedElement: data.Search,
+            queryNotFound: false,
+          });
+        } else {
+          // here i got data.Response = False so cange the state to display allert later
+          console.log(data.Error);
+          this.setState({
+            ...this.state,
+            queryNotFound: true,
+            queryErrorFromApi: data.Error,
+          });
+        }
+      } else {
+        console.log('resp not ok:', resp);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   componentDidMount = async () => {
@@ -54,10 +94,13 @@ export default class App extends Component {
               onChange={this.handleInput}
               className='my-3 '
               type='text'
-              placeholder='Search For Movies'
+              placeholder='Search For Movies and Press Enter'
               value={this.state.query}
             />
           </Form>
+          {this.state.queryNotFound && (
+            <Alert variant='warning'>{this.state.queryErrorFromApi}</Alert>
+          )}
         </Container>
         <Home movies={this.state.movies} />
       </>
